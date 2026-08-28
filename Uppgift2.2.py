240e2db57d6aeee1f6cd9f99e1408dcf6d53d6e2
CoBusselmann <oliver.busselmann@gmail.com>
2026-08-28 14:41:05 +0200

    Övningsuppgifter kapitel 2

--- Uppgift2.2.py
+++ Uppgift2.2.py
@@ -1,16 +1,26 @@
-import math
+"""import math
 x1 = float(input("Ange avstånd för X1: "))
 x2 = float(input("Ange avstånd för X2: "))
 y1 = float(input("Ange avstånd för Y1: "))
 y2 = float(input("Ange avstånd för Y2: "))
 print ("Hej")
 utrakning = math.sqrt((x1 - x2)**2 +  (y1-y2)**2)
-print
 print(f"{utrakning:.2f}")
-"""
+
 import math
 n0 = int(input("Ange startmängd: "))
 t = int(input("Ange hur många år: "))
 lambda_ = math.log(2) / t
 n = n0 * math.exp(-lambda_ * t)
-print(f"{n}")"""
+print(f"{n}")
+
+current_meter = int(input("Ange nuvarande mätarställning: "))
+meters_1_year_ago = int(input("Ange mätarställning för ett år sedan: "))
+liter_tankat = int(input("Ange liter bensin tankat på ett år: "))
+"""
+"""
+korstracka = (current_meter - meters_1_year_ago)
+forbrukning = (liter_tankat / korstracka) 
+print(f"Antal körda mil {korstracka}")
+print(f" Din bil drar ca {forbrukning:.2f} liter per mil")
+"""
