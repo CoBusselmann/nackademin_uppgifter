621d8e9ce8c310e4a9d33e6a4bb3755a12010b87
CoBusselmann <oliver.busselmann@gmail.com>
2026-08-28 14:45:52 +0200

    rm uppgift2.1.py

--- uppgift2.1.py
+++
@@ -1,8 +0,0 @@
-current_meter = int(input("Ange nuvarande mätarställning: "))
-meters_1_year_ago = int(input("Ange mätarställning för ett år sedan: "))
-liter_tankat = int(input("Ange liter bensin tankat på ett år: "))
-
-korstracka = (current_meter - meters_1_year_ago)
-forbrukning = (liter_tankat / korstracka) 
-print(f"Antal körda mil {korstracka}")
-print(f" Din bil drar ca {forbrukning:.2f} liter per mil")
