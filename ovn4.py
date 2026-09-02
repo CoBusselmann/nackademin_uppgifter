e4235c9b285a417c0a081669f644567336632c77
CoBusselmann <oliver.busselmann@gmail.com>
2026-09-02 10:16:36 +0200

    Övningar Kap 4

--- ovn4.py
+++ ovn4.py
@@ -7,7 +7,8 @@ while k < 6:
 k = k + 2
 print("]")
 """
-
+#-----------------------------------------------------------------------------------------------------------------
+"""
 print("---Har Emilie halsfluss eller inte---")
 
 while True:
@@ -23,5 +24,54 @@ while True:
     else:
         print("Du är frisk, vad skönt!!")
         break
+"""
 
-        
+#-------------------------------------------------------------------------------------------------------------
+
+#Uppgift 4.2 
+"""
+tal = int(input("Skriv ett tal: "))
+
+summa = 0
+
+for i in range(1, tal + 1):
+    summa += i**2
+
+print(f"Summan är: {summa}")
+"""
+
+#----------------------------------------------------------------------------------------------------------
+
+#Uppgift 4.4 och 4.5
+
+"""
+hight_droped = float(input("Hur högt upp droppar du bollen(meter): "))
+
+while True:
+    end = 0.01
+    antal_studs = 0 
+    while True:
+        if hight_droped > end:
+            hight_droped  = hight_droped * 0.7
+            antal_studs += 1 
+        elif hight_droped <= 0.01:
+            print(f"Bollen studsade {antal_studs} gånger. ")
+            break
+    hight_droped = float(input("Hur högt upp droppar du bollen(meter): "))
+    if hight_droped < 0:
+        break
+    print("Programmet avslutas")
+"""
+
+storsta = 0
+minsta = 1.e300 # ett stort tal
+while True:
+    tal = float(input('> '))
+    if tal < 0:
+        break
+    if tal > storsta:
+        storsta = tal
+        if tal < minsta:
+            minsta = tal
+    print(f'Största talet: {storsta}')
+    print(f'Minsta talet: {minsta}')
